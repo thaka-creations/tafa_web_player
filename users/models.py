b80a1a71d284130df0ac1c75e333c52df0e5268e
abdulnelfrank <nelfrankaj@gmail.com>
2022-12-20 10:47:19 +0300

    feat: users app updates

--- users/models.py
+++ users/models.py
@@ -1,3 +1,64 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+ACCOUNT_STATUS = [
+    ("ACTIVE", "ACTIVE"),
+    ("DEACTIVATED", "DEACTIVATED"),
+    ("SUSPENDED", "SUSPENDED"),
+]
+
+
+class BaseModel(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    date_deleted = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+    phone_verified = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    account_status = models.CharField(max_length=255, choices=ACCOUNT_STATUS, default="ACTIVE")
+
+    USERNAME_FIELD = "username"
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    class Meta:
+        ordering = ["-date_created"]
+
+
+class PublicUser(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="public_user")
+    school = models.CharField(max_length=1000, blank=True, null=True)
+    county = models.CharField(max_length=255, blank=True, null=True)
+    profile_status = models.CharField(max_length=255, choices=ACCOUNT_STATUS, default="ACTIVE")
+
+
+class Staff(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="staff_user")
+    profile_status = models.CharField(max_length=255, choices=ACCOUNT_STATUS, default="ACTIVE")
