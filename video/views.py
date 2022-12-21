96a32d213673380cbfa11c2214820b3dd7991839
abdulnelfrank <nelfrankaj@gmail.com>
2022-12-21 08:38:15 +0300

    encryption, decription video keys

--- video/views.py
+++ video/views.py
@@ -13,9 +13,34 @@ class VideoViewSet(viewsets.ViewSet):
     )
     def keygen(self, request):
         # key generation
-        try:
-            utils.keygen()
-        except Exception as e:
-            return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-        return Response({"message": "Key generated successfully"}, status=status.HTTP_200_OK)
+        resp = utils.keygen()
+        if not resp:
+            return Response({"message": "Error generating key"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"key": str(resp)}, status=status.HTTP_200_OK)
+
+    @action(
+        methods=['POST'],
+        detail=False,
+        url_path='encrypt-file',
+        url_name='encrypt-file'
+    )
+    def encrypt_file(self, request):
+        # encrypt file
+        resp = utils.encrypt_file(request.data['key'])
+        if not resp:
+            return Response({"message": "Error encrypting file"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "File encrypted successfully"}, status=status.HTTP_200_OK)
+
+    @action(
+        methods=['POST'],
+        detail=False,
+        url_path='decrypt-file',
+        url_name='decrypt-file'
+    )
+    def decrypt_file(self, request):
+        # decrypt file
+        resp = utils.decrypt_file(request.data['key'])
+        if not resp:
+            return Response({"message": "Error decrypting file"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "File decrypted successfully"}, status=status.HTTP_200_OK)
 
