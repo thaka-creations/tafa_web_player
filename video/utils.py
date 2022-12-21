6fe5f031e44b6ebc64db40c5645f87c1c7ba1bd7
abdulnelfrank <nelfrankaj@gmail.com>
2022-12-21 08:26:31 +0300

    feat: key file generation, encryption and decription

--- video/utils.py
+++ video/utils.py
@@ -2,42 +2,72 @@ from cryptography.fernet import Fernet
 
 
 # key generation
-def key_gen():
-    key = Fernet.generate_key()
+def keygen():
+    try:
+        key = Fernet.generate_key()
 
-    # string the key in a file
-    with open('file.key', 'wb') as file_key:
-        file_key.write(key)
+        # string the key in a file
+        with open('file.key', 'wb') as file_key:
+            file_key.write(key)
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
+
+# read key file
+def read_key():
+    try:
+        with open('file.key', 'rb') as file_key:
+            key = file_key.read()
+        return key
+    except Exception as e:
+        print(e)
+        return False
+
+
+# encrypt file
+def encrypt_file():
+    key = read_key()
+    if not key:
+        return False
     fernet = Fernet(key)
 
-    with open('nba.csv', 'rb') as file:
-        original = file.read()
+    try:
+        with open('nba.csv', 'rb') as file:
+            original = file.read()
+        encrypted = fernet.encrypt(original)
 
-    encrypted = fernet.encrypt(original)
+        # opening file in write mode and writing encrypted data
+        with open('nba.csv', 'wb') as encrypted_file:
+            encrypted_file.write(encrypted)
+        return True
+    except Exception as e:
+        print(e)
+        return False
 
-    # opening the file in write mode and
-    # writing the encrypted data
-    with open('nba.csv', 'wb') as encrypted_file:
-        encrypted_file.write(encrypted)
 
-    return True
-
-
-def decrypt():
+# decrypt file
+def decrypt_file():
     # read key
-    with open('file.key', 'rb') as file_key:
-        key = file_key.read()
-
+    key = read_key()
+    if not key:
+        return False
     fernet = Fernet(key)
 
-    with open('nba.csv', 'rb') as file:
-        encrypted = file.read()
+    try:
 
-    # decrypting the file
-    decrypted = fernet.decrypt(encrypted)
+        with open('nba.csv', 'rb') as file:
+            encrypted = file.read()
 
-    # opening the file in write mode and
-    # writing the decrypted data
-    with open('nba.csv', 'wb') as decrypted_file:
-        decrypted_file.write(decrypted)
+        # decrypting the file
+        decrypted = fernet.decrypt(encrypted)
+
+        # opening the file in write mode and
+        # writing the decrypted data
+        with open('nba.csv', 'wb') as decrypted_file:
+            decrypted_file.write(decrypted)
+        return True
+    except Exception as e:
+        print(e)
+        return False
