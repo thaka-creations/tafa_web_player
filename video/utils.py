329d58463d420f34c61cf7e08014a3b9313c81ee
abdulnelfrank <nelfrankaj@gmail.com>
2022-12-22 08:22:22 +0300

    feat: added opacity to text watermark

--- video/utils.py
+++ video/utils.py
@@ -1,3 +1,4 @@
+import random
 from cryptography.fernet import Fernet
 from moviepy.editor import *
 
@@ -53,13 +54,49 @@ def decrypt_file(key):
 
 
 def add_watermark():
+    # Load the video file
     try:
-        video = VideoFileClip('test.mp4')
-        txt_clip = TextClip("Watermark", fontsize=70, color='red')
-        txt_clip = txt_clip.set_position("center").set_duration(video.duration)
-        final = CompositeVideoClip([video, txt_clip])
-        final.write_videofile('test1.mp4')
+        video = VideoFileClip("test.mp4")
+
+        text_clip = (TextClip("My Watermark", fontsize=70, color='white')
+                     .set_duration(video.duration)).set_opacity(0.5)
+
+        # Define a function to return the watermark position at each frame
+        def watermark_position(t):
+            # Set the speed and direction of the movement
+            speed_x = 50
+            speed_y = 30
+            direction_x = 1
+            direction_y = -1
+
+            # Calculate the position at the current time
+            x = (video.w / 2) + (speed_x * t * direction_x)
+            y = (video.h / 2) + (speed_y * t * direction_y)
+
+            # Keep the watermark within the bounds of the screen
+            if x < 0:
+                x = 0
+                direction_x = 1
+            elif x > video.w - text_clip.w:
+                x = video.w - text_clip.w
+                direction_x = -1
+
+            if y < 0:
+                y = 0
+                direction_y = -1
+            elif y > video.h - text_clip.h:
+                y = video.h - text_clip.h
+                direction_y = -1
+
+            return x, y
+
+        # Add the watermark to the video
+        watermarked_video = CompositeVideoClip([video, text_clip.set_pos(watermark_position)])
+
+        # Save the watermarked video to a new file
+        watermarked_video.write_videofile("watermarked_video.mp4")
         return True
     except Exception as e:
-        print("Error watermarking video", e)
+        print(e)
         return False
+
