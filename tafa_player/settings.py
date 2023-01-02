abcbb753b60a8c2256d2e0a0b7a01df73a61f2ed
abdulnelfrank <nelfrankaj@gmail.com>
2023-01-02 11:54:02 +0300

    feat: added datatables to requirements

--- tafa_player/settings.py
+++ tafa_player/settings.py
@@ -156,6 +156,16 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = "/var/www/static/"
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- tafa_player/urls.py
+++ tafa_player/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 api_version = "v1"
 
@@ -23,3 +25,7 @@ urlpatterns = [
     path(f"api/{api_version}/", include("video.urls")),
     path("web/admin/", include("staff.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
